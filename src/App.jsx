80ebc378590c95f53c08557ed619184af7719e6b
Esther <125416224+estheralv@users.noreply.github.com>
2024-11-04 20:44:02 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Saudacao from "./assets/components/Saudacao"
+import Frase from "./assets/components/Frase"
+import ListaOrdenada from "./assets/components/ListaOrdenada"
+import Garagem from './assets/components/Garagem'
+import Contador from './assets/components/Contador'
+import ListadeTarefas from './assets/components/ListadeTarefas'
+import ConversordeMoedas from './assets/components/ConversordeMoedas'
+import Calculadora from './assets/components/Calculadora'
+import GaleriaDeImagens from './assets/components/GaleriaDeImagens'
+import ListaDeContatos from './assets/components/ListaDeContatos'
+import BuscaDeFilmes from './assets/components/BuscaDeFilmes'
+import Cronometro from './assets/components/Cronometro'
+
+function App() {
+  return (
+    <>
+    <Cronometro/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Quiz.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+
+const perguntas = [
+    {
+        pergunta: 'Qual é a capital do Brasil?',
+        opcoes: ['Rio de Janeiro', 'São Paulo', 'Brasília', 'Belo Horizonte'],
+        resposta: 'Brasília'
+    },
+    {
+        pergunta: 'Quem descobriu o Brasil?',
+        opcoes: ['Pedro Álvares Cabral', 'Cristovão Colombo', 'Vasco da gama', 'Fernão de magalhães'],
+        resposta: 'Pedro Álvares Cabral'
+    },
+    {
+        pergunta: 'Quantos planetas existem no sistema solar?',
+        opcoes: ['5', '7', '9', '8'],
+        resposta: '8'
+    }
+]
